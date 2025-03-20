99cacd07412ee204b77cdeabb11a21d9e8ab7563
Graham Hukill <ghukill@gmail.com>
2025-03-20 14:57:55 -0400

    Create Config class and align with lambda module

--- lambdas/validator.py
+++ lambdas/validator.py
@@ -1,32 +1,19 @@
 import json
 import logging
-import os
 
-import sentry_sdk
-from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
+from lambdas.config import Config, check_verbosity, configure_logger
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-env = os.getenv("WORKSPACE")
-if sentry_dsn := os.getenv("SENTRY_DSN"):
-    sentry = sentry_sdk.init(
-        dsn=sentry_dsn,
-        environment=env,
-        integrations=[
-            AwsLambdaIntegration(),
-        ],
-        traces_sample_rate=1.0,
-    )
-    logger.info("Sentry DSN found, exceptions will be sent to Sentry with env=%s", env)
-else:
-    logger.info("No Sentry DSN found, exceptions will not be sent to Sentry")
+CONFIG = Config()
 
 
 def lambda_handler(event: dict) -> str:
-    if not os.getenv("WORKSPACE"):
-        unset_workspace_error_message = "Required env variable WORKSPACE is not set"
-        raise RuntimeError(unset_workspace_error_message)
+    """AWS Lambda entrypoint."""
+    CONFIG.check_required_env_vars()
+    verbose = check_verbosity(event.get("verbose", False))
+    configure_logger(logging.getLogger(), verbose=verbose)
 
     logger.debug(json.dumps(event))
 
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,13 +1,12 @@
-from importlib import reload
-
 import pytest
 
 from lambdas import validator
+from lambdas.config import configure_sentry
 
 
 def test_validator_configures_sentry_if_dsn_present(caplog, monkeypatch):
     monkeypatch.setenv("SENTRY_DSN", "https://1234567890@00000.ingest.sentry.io/123456")
-    reload(validator)
+    configure_sentry()
     assert (
         "Sentry DSN found, exceptions will be sent to Sentry with env=test" in caplog.text
     )
@@ -15,15 +14,17 @@ def test_validator_configures_sentry_if_dsn_present(caplog, monkeypatch):
 
 def test_validator_doesnt_configure_sentry_if_dsn_not_present(caplog, monkeypatch):
     monkeypatch.delenv("SENTRY_DSN", raising=False)
-    reload(validator)
+    configure_sentry()
     assert "No Sentry DSN found, exceptions will not be sent to Sentry" in caplog.text
 
 
 def test_lambda_handler_missing_workspace_env_raises_error(monkeypatch):
     monkeypatch.delenv("WORKSPACE", raising=False)
-    with pytest.raises(RuntimeError) as error:
+    with pytest.raises(
+        OSError,
+        match="Missing required environment variables: WORKSPACE",
+    ):
         validator.lambda_handler({})
-    assert "Required env variable WORKSPACE is not set" in str(error)
 
 
 def test_validator():
