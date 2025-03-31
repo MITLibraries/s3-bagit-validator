1cd363e11bfdfea4f3f90192ef9d008c69c9d7bb
Graham Hukill <ghukill@gmail.com>
2025-03-31 08:51:57 -0400

    Optimize Athena SQL query
    Why these changes are being introduced:
    
    It was noticed that an Athena query was scanning 10mb+ of data
    for a query that felt like it could have scanned less.
    
    How this addresses that need:
    * Use 'union all' instead of 'union'
    * Use window function 'row_number()' instead of a self join
    
    Side effects of this change:
    * Less data scanned per query, less money
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IR-193

--- lambdas/sql/s3_inventory.sql
+++ lambdas/sql/s3_inventory.sql
@@ -1,32 +1,25 @@
 with cdps_aip_inventory as (
     select * from "aip1b-data"
-    union select * from "aip2b-data"
-    union select * from "aip3b-data"
-    union select * from "aip4b-data"
-    union select * from "aip5b-data"
+    union all select * from "aip2b-data"
+    union all select * from "aip3b-data"
+    union all select * from "aip4b-data"
+    union all select * from "aip5b-data"
 ),
-aip_files as (
-    select * from cdps_aip_inventory
+date_ordered_files as (
+    select
+        *,
+        row_number() over (partition by key order by last_modified_date desc) as rn
+    from cdps_aip_inventory
     where key like %(s3_key_prefix)s
-    -- the following utilizes the hive 'dt' partition, limiting data scanned
     and (
         parse_datetime(dt, 'yyyy-MM-dd-HH-mm')
         between date_add('day', -3, current_timestamp) and current_timestamp
     )
-),
-latest_file_dates as (
-    select
-        key,
-        max(last_modified_date) as max_date
-    from aip_files
-    where is_latest = true and is_delete_marker = false
-    group by key
 )
 select
-    af.key,
-    af.checksum_algorithm
-from aip_files af
-inner join latest_file_dates lf on
-    af.key = lf.key and
-    af.last_modified_date = lf.max_date
-;
+    key,
+    checksum_algorithm
+from date_ordered_files
+where rn = 1
+and is_latest = true
+and is_delete_marker = false
