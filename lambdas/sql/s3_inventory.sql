4f566d3093ce7ed603d06726a166d99a12dfb2a6
Graham Hukill <ghukill@gmail.com>
2025-04-01 10:39:04 -0400

    Improve Athena query for AIP inventory data
    Why these changes are being introduced:
    
    The updated AIP inventory Athena query has a potentially
    non-deterministic quality where S3 inventory rows may not
    have originated from the most recent S3 inventory pull
    (which is partitioned by date).  Though the data may
    have been accurate, it felt a little sloppy.
    
    How this addresses that need:
    
    Instead of a window function, we focus on retrieving only
    the most recent data pull via the most recent 'dt' partition.
    Additionally, if we know a) it is the most recent data pull
    and b) it is 'is_latest' then we don't need to sort by
    last_modified_date anymore.
    
    Confirmed that query time remains about the same, but data
    scanned is reduced, suggesting that use of partitions is more
    effective here.
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IR-193

--- lambdas/sql/s3_inventory.sql
+++ lambdas/sql/s3_inventory.sql
@@ -1,25 +1,25 @@
+-- union all CDPS inventory tables
 with cdps_aip_inventory as (
     select * from "aip1b-data"
     union all select * from "aip2b-data"
     union all select * from "aip3b-data"
     union all select * from "aip4b-data"
     union all select * from "aip5b-data"
-),
-date_ordered_files as (
-    select
-        *,
-        row_number() over (partition by key order by last_modified_date desc) as rn
-    from cdps_aip_inventory
-    where key like %(s3_key_prefix)s
-    and (
-        parse_datetime(dt, 'yyyy-MM-dd-HH-mm')
-        between date_add('day', -3, current_timestamp) and current_timestamp
-    )
 )
 select
+    parse_datetime(dt, 'yyyy-MM-dd-HH-mm') as s3_inventory_date,
+    from_unixtime(last_modified_date / 1000) as last_modified,
     key,
     checksum_algorithm
-from date_ordered_files
-where rn = 1
-and is_latest = true
-and is_delete_marker = false
+from cdps_aip_inventory as cai
+
+-- ensures data is coming from last S3 inventory pull parquet file(s)
+where dt = (select max(dt) as dt from cdps_aip_inventory)
+
+-- ensures data is only for most recent form of record and not deleted
+and is_latest
+and not is_delete_marker
+
+-- lastly, filter to our AIP prefix
+and key like %(s3_key_prefix)s
+;
