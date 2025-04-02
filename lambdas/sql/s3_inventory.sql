cef3bf4f69121d6e2cdd421ddc37770cfab1fc24
Graham Hukill <ghukill@gmail.com>
2025-04-02 08:29:15 -0400

    Get most recent partition of each glue table
    Why these changes are being introduced:
    
    An edge case was discovered where, if the query was run
    during an S3 inventory data load, the value of 'max(dt)'
    could be the value from a Glue table that had already been
    updated, but others had not.  In this way, it would look
    like -- for that dt partition -- that the files didn't exist.
    
    How this addresses that need:
    
    This simplifies the query by filtering each glue table
    immediately to the most recent dt partition.
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IR-193

--- lambdas/sql/s3_inventory.sql
+++ lambdas/sql/s3_inventory.sql
@@ -1,10 +1,11 @@
--- union all CDPS inventory tables
+-- union all CDPS inventory tables,
+-- limiting to most recent S3 Inventory pull as identified by 'dt' partition
 with cdps_aip_inventory as (
-    select * from "aip1b-data"
-    union all select * from "aip2b-data"
-    union all select * from "aip3b-data"
-    union all select * from "aip4b-data"
-    union all select * from "aip5b-data"
+    select * from "aip1b-data" where dt = (select max(dt) from "aip1b-data")
+    union all select * from "aip2b-data" where dt = (select max(dt) from "aip2b-data")
+    union all select * from "aip3b-data" where dt = (select max(dt) from "aip3b-data")
+    union all select * from "aip4b-data" where dt = (select max(dt) from "aip4b-data")
+    union all select * from "aip5b-data" where dt = (select max(dt) from "aip5b-data")
 )
 select
     parse_datetime(dt, 'yyyy-MM-dd-HH-mm') as s3_inventory_date,
@@ -13,11 +14,8 @@ select
     checksum_algorithm
 from cdps_aip_inventory as cai
 
--- ensures data is coming from last S3 inventory pull parquet file(s)
-where dt = (select max(dt) as dt from cdps_aip_inventory)
-
 -- ensures data is only for most recent form of record and not deleted
-and is_latest
+where is_latest
 and not is_delete_marker
 
 -- lastly, filter to our AIP prefix
